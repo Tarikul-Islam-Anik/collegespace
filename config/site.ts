db50ee13d03b58faba1222b9857aa9ed2cef59c0
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-31 16:08:32 +0600

    Initial commit.

---
+++ config/site.ts
@@ -0,0 +1,9 @@
+export const siteConfig = {
+  name: 'College Space',
+  url: 'https://collegespaces.vercel.app',
+  ogImage: 'https://collegespaces.vercel.app/og.jpg',
+  description:
+    'CollegeSpace brings together the brightest minds and operators across the world to solve complex problems and build the future.',
+};
+
+export type SiteConfig = typeof siteConfig;

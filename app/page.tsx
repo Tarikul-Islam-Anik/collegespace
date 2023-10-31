4a06e15590b3547366a06b3254c3f0eabfa18475
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-31 17:07:57 +0600

    Next auth added.

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,24 @@
+import Link from 'next/link';
+import { authOptions } from '@/lib/auth-options';
+import { getServerSession } from 'next-auth';
+
 export default async function Home() {
-  return <h1>Hello World</h1>;
+  const session = await getServerSession(authOptions);
+  return (
+    <main className='flex min-h-screen flex-col items-center justify-center'>
+      <div className='flex flex-col items-center gap-2'>
+        <div className='flex flex-col items-center justify-center gap-4'>
+          <p className='text-center text-2xl'>
+            {session && <span>Logged in as {session.user?.name}</span>}
+          </p>
+          <Link
+            href={session ? '/api/auth/signout' : '/api/auth/signin'}
+            className='rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20'
+          >
+            {session ? 'Sign out' : 'Sign in'}
+          </Link>
+        </div>
+      </div>
+    </main>
+  );
 }

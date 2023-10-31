219e2434cad1b6a99bf37bbd534aa489df058621
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-31 18:59:38 +0600

    Layout components added.

---
+++ components/layout/navbar/navlink.tsx
@@ -0,0 +1,43 @@
+import NextLink from 'next/link';
+import { Text } from '@radix-ui/themes';
+import {
+  NavigationMenuLink,
+  navigationMenuTriggerStyle,
+} from '@/components/ui/navigation-menu';
+import ScreenReaderOnly from '@/components/ui/screen-reader-only';
+import { cn } from '@/lib/utils';
+
+const NavLink = ({
+  href,
+  label,
+  children,
+  ...props
+}: {
+  href: string;
+  label: string;
+  children: React.ReactNode;
+}) => {
+  return (
+    <NextLink
+      href={href}
+      passHref
+      legacyBehavior
+      aria-label={(label as string) + ' page'}
+    >
+      <NavigationMenuLink
+        className={cn(navigationMenuTriggerStyle(), 'h-12 w-12')}
+        aria-label={(label as string) + ' page'}
+        {...props}
+      >
+        <Text>
+          {children}
+          <ScreenReaderOnly content={label + ' page'} />
+        </Text>
+      </NavigationMenuLink>
+    </NextLink>
+  );
+};
+
+NavLink.displayName = 'NavLink';
+
+export default NavLink;

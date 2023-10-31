59b965a935f1b61ba4ff97fb5dbf12d7f331dee1
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-10-31 18:02:01 +0600

    Next auth prisma adapter and login with credentials.

--- lib/auth-options.ts
+++ lib/auth-options.ts
@@ -1,8 +1,44 @@
+import { compare } from 'bcryptjs';
+import prisma from './prisma';
 import { NextAuthOptions } from 'next-auth';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
 import GoogleProvider from 'next-auth/providers/google';
+import CredentialsProvider from 'next-auth/providers/credentials';
 
 export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
   providers: [
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'Email',
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          placeholder: 'Password',
+        },
+      },
+      async authorize(credentials, req) {
+        if (!credentials?.email || !credentials.password) return null;
+
+        const user = await prisma.user.findUnique({
+          where: { email: credentials.email },
+        });
+
+        if (!user) return null;
+
+        const passwordsMatch = await compare(
+          credentials.password,
+          user.password!
+        );
+
+        return passwordsMatch ? user : null;
+      },
+    }),
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,

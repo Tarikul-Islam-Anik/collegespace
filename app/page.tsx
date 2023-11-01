d1dfd376ed9ee21e7d5dfb02ae504663d64f0df7
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-01 19:02:57 +0600

    Home component.

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,5 @@
-import Link from 'next/link';
-import { authOptions } from '@/lib/auth-options';
-import { getServerSession } from 'next-auth';
+import Home from './components/home';
 
-export default async function Home() {
-  const session = await getServerSession(authOptions);
-  return (
-    <main className='flex min-h-screen flex-col items-center justify-center'>
-      <div className='flex flex-col items-center gap-2'>
-        <div className='flex flex-col items-center justify-center gap-4'>
-          <p className='text-center text-2xl'>
-            {session && <span>Logged in as {session.user?.name}</span>}
-          </p>
-          <Link
-            href={session ? '/api/auth/signout' : '/api/auth/signin'}
-            className='rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20'
-          >
-            {session ? 'Sign out' : 'Sign in'}
-          </Link>
-        </div>
-      </div>
-    </main>
-  );
+export default async function HomePage() {
+  return <Home />;
 }

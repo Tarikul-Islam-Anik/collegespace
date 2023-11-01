349674a1e1469fbb8ddc952d69752f9b967fe91a
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-01 19:00:18 +0600

    Jotai provider.

--- context/providers.tsx
+++ context/providers.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import '@radix-ui/themes/styles.css';
 import './radix-theme-config.css';
+import { Provider } from 'jotai';
 import { Theme } from '@radix-ui/themes';
 import AuthProvider from './auth-context';
 import { ThemeProvider } from './theme-provider';
@@ -7,14 +10,16 @@ import { ThemeProvider } from './theme-provider';
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <AuthProvider>
-      <ThemeProvider
-        attribute='class'
-        defaultTheme='system'
-        enableSystem
-        disableTransitionOnChange
-      >
-        <Theme>{children}</Theme>
-      </ThemeProvider>
+      <Provider>
+        <ThemeProvider
+          attribute='class'
+          defaultTheme='system'
+          enableSystem
+          disableTransitionOnChange
+        >
+          <Theme>{children}</Theme>
+        </ThemeProvider>
+      </Provider>
     </AuthProvider>
   );
 };

5214d726c0cea638819da651a1c1bd5246fe7315
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-01 19:03:39 +0600

    Posts route.

---
+++ app/api/posts/[id]/route.ts
@@ -0,0 +1,34 @@
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { id: string } }
+) {
+  const post = await prisma.post.findUnique({
+    where: { id: params.id },
+    select: {
+      id: true,
+      type: true,
+      content: true,
+      createdAt: true,
+      user: {
+        select: {
+          id: true,
+          name: true,
+          image: true,
+        },
+      },
+      likes: {
+        select: {
+          userId: true,
+        },
+      },
+    },
+  });
+
+  if (!post)
+    return NextResponse.json({ error: 'Post not found' }, { status: 404 });
+
+  return NextResponse.json(post, { status: 200 });
+}
---
+++ app/api/posts/route.ts
@@ -0,0 +1,32 @@
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+import authStatus from '@/lib/auth-status';
+
+export async function GET(request: NextRequest) {
+  const { currentUser } = await authStatus();
+
+  const posts = await prisma.post.findMany({
+    where: {
+      userId: currentUser.id,
+    },
+    include: {
+      user: {
+        select: {
+          id: true,
+          name: true,
+          image: true,
+        },
+      },
+      likes: {
+        select: {
+          userId: true,
+        },
+      },
+    },
+    orderBy: {
+      createdAt: 'desc',
+    },
+  });
+
+  return NextResponse.json(posts, { status: 200 });
+}

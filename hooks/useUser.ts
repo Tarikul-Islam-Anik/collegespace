38a6f346ed2b276a51948b4738bba557f3fdd4d5
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-04 14:15:09 +0600

    Add next-auth/react dependency and update useUser hook to handle session and user ID

--- hooks/useUser.ts
+++ hooks/useUser.ts
@@ -1,19 +1,42 @@
 import useSWR from 'swr';
+import { useSession } from 'next-auth/react';
 import { User } from '@/lib/type';
 import fetcher from '@/lib/fetcher';
 
-const useUser = (userId: string) => {
-  const { data, error, isLoading, mutate } = useSWR<User>(
-    `/api/users/${userId}`,
-    fetcher
-  );
+const useUser = (userId?: string) => {
+  const { data: session } = useSession();
 
-  return {
-    data,
-    error,
-    isLoading,
-    mutate,
-  };
+  if (userId) {
+    const {
+      data: user,
+      error,
+      isLoading,
+      mutate,
+    } = useSWR<User>(`/api/users/${userId}`, fetcher);
+    return {
+      user,
+      error,
+      isLoading,
+      mutate,
+    };
+  } else {
+    const {
+      data: user,
+      error,
+      isLoading,
+      mutate,
+    } = useSWR<User>(
+      // @ts-ignore
+      `/api/users/${session?.user?.id}`,
+      fetcher
+    );
+    return {
+      user,
+      error,
+      isLoading,
+      mutate,
+    };
+  }
 };
 
 export default useUser;

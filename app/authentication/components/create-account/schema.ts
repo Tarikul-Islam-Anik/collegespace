53b5b153e62c8d61fd509e5e0080991df1c2e6f8
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-04 18:26:48 +0600

    Add username validation to CreateAccountFormSchema

--- app/authentication/components/create-account/schema.ts
+++ app/authentication/components/create-account/schema.ts
@@ -4,6 +4,17 @@ export const CreateAccountFormSchema = z.object({
   name: z
     .string()
     .min(3, { message: 'Name must be at least 3 characters long' }),
+  username: z
+    .string()
+    .min(2, {
+      message: 'Name must be at least 2 characters.',
+    })
+    .max(30, {
+      message: 'Name must not be longer than 30 characters.',
+    })
+    .regex(/^[a-z0-9]+$/, {
+      message: 'Username must only contain lowercase alphanumeric characters.',
+    }),
   email: z.string().email({ message: 'Invalid email address' }),
   phone: z.string(),
   password: z

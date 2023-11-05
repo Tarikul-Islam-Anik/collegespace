d5ecbfedba4942f205f5041f14b09d8a16715b91
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-05 15:17:46 +0600

    Add new types to type.ts

--- lib/type.ts
+++ lib/type.ts
@@ -1,8 +1,24 @@
-import type { Post, User, Like, Job } from '@prisma/client';
+import type {
+  Post,
+  User,
+  Like,
+  Job,
+  StudentDetails,
+  EducationalInformation,
+  Project,
+} from '@prisma/client';
 
 export type PostType = Post & {
   likes: Like[];
   user: User;
 };
 
-export type { User, Post, Like, Job };
+export type {
+  User,
+  Post,
+  Like,
+  Job,
+  StudentDetails,
+  EducationalInformation,
+  Project,
+};

bae72a109e701da2a72a22fa4fe25dcb976fd7ec
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-06 14:05:32 +0600

    Update UserAtom type in atom.ts

--- lib/atom.ts
+++ lib/atom.ts
@@ -1,8 +1,8 @@
 import { atom } from 'jotai';
 import { atomWithStorage } from 'jotai/utils';
-import { User, PostType } from './type';
+import { UserType, PostType } from './type';
 
-const UserAtom = atomWithStorage<User | null>('user', null);
+const UserAtom = atomWithStorage<UserType | null>('user', null);
 const PostsAtom = atom<PostType[] | null>(null);
 
 export { UserAtom, PostsAtom };

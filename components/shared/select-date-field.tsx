db3e60f54cf62c91e92a56166d5e4f5f6e163d4a
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-06 16:52:42 +0600

    Add SelectDateField component for date selection.

---
+++ components/shared/select-date-field.tsx
@@ -0,0 +1,76 @@
+import { cn } from '@/lib/utils';
+import { format } from 'date-fns';
+import { Calendar as CalenderIcon } from 'iconsax-react';
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from '@/components/ui/form';
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover';
+import { Button } from '@/components/ui/button';
+import { Calendar } from '@/components/ui/calendar';
+
+interface SelectDateFieldProps {
+  form: any;
+  name: string;
+  label: string;
+}
+
+const SelectDateField = ({ form, name, label }: SelectDateFieldProps) => {
+  return (
+    <FormField
+      control={form.control}
+      name={name}
+      render={({ field }) => (
+        <FormItem className='flex flex-col'>
+          <FormLabel>{label}</FormLabel>
+          <Popover>
+            <PopoverTrigger asChild>
+              <FormControl>
+                <Button
+                  variant={'outline'}
+                  className={cn(
+                    'w-[240px] pl-3 text-left font-normal',
+                    !field.value && 'text-muted-foreground'
+                  )}
+                >
+                  {field.value ? (
+                    format(new Date(field.value), 'PPP')
+                  ) : (
+                    <span>Pick a date</span>
+                  )}
+                  <CalenderIcon className='ml-auto h-4 w-4 opacity-50' />
+                </Button>
+              </FormControl>
+            </PopoverTrigger>
+            <PopoverContent className='w-auto p-0' align='start'>
+              <Calendar
+                mode='single'
+                selected={new Date(field.value ?? '')}
+                onSelect={field.onChange}
+                disabled={(date) =>
+                  date > new Date() || date < new Date('1900-01-01')
+                }
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+          <FormDescription>
+            This won’t be part of your public profile.
+          </FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  );
+};
+
+SelectDateField.displayName = 'SelectDateField';
+export default SelectDateField;

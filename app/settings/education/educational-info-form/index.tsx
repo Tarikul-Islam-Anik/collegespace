4acae462cdbe50796117ae5003c557d75ecd87c7
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-10 22:21:57 +0600

    Add educational info form fields and schema

---
+++ app/settings/education/educational-info-form/index.tsx
@@ -0,0 +1,76 @@
+'use client';
+
+import { toast } from 'sonner';
+import { useForm } from 'react-hook-form';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { Form } from '@/components/ui/form';
+import { Button } from '@/components/ui/button';
+import FormFields from './form-fields';
+import { educationalInfoFormSchema, EducationalInfoFormValues } from './schema';
+import { Flex } from '@/components/layout/flex';
+
+const EducationalInfoForm = ({
+  setOpen,
+}: {
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  const { currentUser, setCurrentUser } = useCurrentUser();
+  const defaultValues: Partial<EducationalInfoFormValues> = {
+    school: '',
+    degree: '',
+    field: '',
+    grade: 0.0,
+  };
+
+  const form = useForm<EducationalInfoFormValues>({
+    resolver: zodResolver(educationalInfoFormSchema),
+    defaultValues,
+    mode: 'onChange',
+  });
+
+  function onSubmit(data: EducationalInfoFormValues) {
+    toast.promise(
+      fetch('/api/education/new', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+      }).then((res) => res.json()),
+      {
+        loading: 'Adding educational information...',
+        success: (data) => {
+          setCurrentUser((prev) => {
+            prev?.StudentDetails.education.push(data);
+            return prev;
+          });
+          setOpen(false);
+          return 'Information added successfully!';
+        },
+        error: 'Something went wrong!',
+      }
+    );
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)}>
+        <FormFields form={form} />
+        <Flex justify='end' mt={4} gap={4}>
+          <Button
+            type='button'
+            variant='secondary'
+            onClick={() => form.reset()}
+          >
+            Clear
+          </Button>
+          <Button type='submit'>Update</Button>
+        </Flex>
+      </form>
+    </Form>
+  );
+};
+
+EducationalInfoForm.displayName = 'EducationalInfoForm';
+export default EducationalInfoForm;

fe4a455a13bb2fd908b6b94c7b601c0f0c1a636a
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-10 12:12:41 +0600

    Add section heading component and refactor settings pages.

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -1,21 +1,14 @@
 import { Box } from '@/components/layout/box';
-import { Text } from '@/components/typography/text';
 import ProfileForm from './components/profile-form';
-import { Separator } from '@/components/ui/separator';
-import { Heading } from '@/components/typography/heading';
+import SectionHeading from './components/section-heading';
 
 export default function SettingsProfilePage() {
-
   return (
     <Box className='space-y-6'>
-      <Box>
-        <Heading className='text-lg font-medium'>Profile</Heading>
-        <Text className='text-sm text-muted-foreground'>
-          This information will be displayed publicly so be careful what you
-          share.
-        </Text>
-      </Box>
-      <Separator />
+      <SectionHeading
+        title='Profile'
+        description='This information will be displayed publicly so be careful what you share.'
+      />
       <ProfileForm />
     </Box>
   );

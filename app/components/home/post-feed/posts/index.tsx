64f12d0a9e3b9a11b6869bb7feda3109d7fc6d52
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-13 19:07:04 +0600

    Add message for no posts found in Posts component.

--- app/components/home/post-feed/posts/index.tsx
+++ app/components/home/post-feed/posts/index.tsx
@@ -5,12 +5,19 @@ import { PostsAtom } from '@/lib/atom';
 import usePosts from '@/hooks/usePosts';
 import PostItem from './post-item';
 import Loading from '@/components/shared/loader';
+import { Text } from '@/components/typography/text';
 
 const Posts = () => {
   const { data = [], isLoading } = usePosts();
   const [posts, setPosts] = useAtom(PostsAtom);
 
   if (isLoading) return <Loading />;
+  else if (posts?.length === 0)
+    return (
+      <Text size='sm' align='center' color='muted-foreground' className='mt-8'>
+        No posts found.
+      </Text>
+    );
   else {
     setPosts(data);
     return (

879067e14842da87265b8434c4c11a3bec1ad007
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-13 19:48:56 +0600

    Add Company field to user API.

---
+++ app/api/company/route.ts
@@ -0,0 +1,32 @@
+import { NextRequest, NextResponse } from 'next/server';
+import prisma from '@/lib/prisma';
+import authStatus from '@/lib/auth-status';
+
+export async function PATCH(request: NextRequest) {
+  const body = await request.json();
+  const { currentUser } = await authStatus();
+
+  const companyExists = await prisma.company.findUnique({
+    where: {
+      id: currentUser.Company[0].id,
+    },
+  });
+
+  if (companyExists) {
+    const company = await prisma.company.update({
+      where: {
+        id: currentUser.Company[0].id,
+      },
+      data: { ...body },
+    });
+    return NextResponse.json(company, { status: 200 });
+  } else {
+    const company = await prisma.company.create({
+      data: {
+        ...body,
+        owner: { connect: { id: currentUser.id } },
+      },
+    });
+    return NextResponse.json(company, { status: 201 });
+  }
+}

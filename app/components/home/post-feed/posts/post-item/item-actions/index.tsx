41765d27f12838655b7bbd3694f2024c07a01930
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-14 18:12:48 +0600

    Refactor item-actions component to export individual action handlers.

--- app/components/home/post-feed/posts/post-item/item-actions/index.tsx
+++ app/components/home/post-feed/posts/post-item/item-actions/index.tsx
@@ -1,34 +1,3 @@
-import HandleLike from './handleLike';
-import HandleRepost from './handleRepost';
-import HandleComment from './handleComment';
-import { Flex } from '@/components/layout/flex';
-import ToolTipParent from '@/components/shared/tooltip-parent';
-
-const actions = [
-  {
-    label: 'Like',
-    handle: HandleLike,
-  },
-  {
-    label: 'Repost',
-    handle: HandleRepost,
-  },
-  {
-    label: 'Comment',
-    handle: HandleComment,
-  },
-];
-
-const PostActions = ({ postId }: { postId: string }) => {
-  return (
-    <Flex>
-      {actions.map(({ handle, label }) => (
-        <ToolTipParent content={label} key={label}>
-          {handle({ postId })}
-        </ToolTipParent>
-      ))}
-    </Flex>
-  );
-};
-
-export default PostActions;
+export { default as HandleLike } from './handleLike';
+export { default as HandleRepost } from './handleRepost';
+export { default as HandleComment } from './handleComment';

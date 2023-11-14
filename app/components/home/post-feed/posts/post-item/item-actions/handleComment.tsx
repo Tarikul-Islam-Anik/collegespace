bfc43ab5e9bd62ead6ff2c56f417bdfd01bb16aa
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-14 18:12:59 +0600

    Add CommentDialog component to HandleComment

--- app/components/home/post-feed/posts/post-item/item-actions/handleComment.tsx
+++ app/components/home/post-feed/posts/post-item/item-actions/handleComment.tsx
@@ -1,13 +1,16 @@
 import { MessageText } from 'iconsax-react';
 import { Button } from '@/components/ui/button';
 import ScreenReaderOnly from '@/components/ui/screen-reader-only';
+import CommentDialog from '../../comment/comment-dialog';
 
-const HandleComment = () => {
+const HandleComment = ({ postContent }: { postContent: React.ReactNode }) => {
   return (
-    <Button variant='ghost' size='icon'>
-      <MessageText className='text-muted-foreground' />
-      <ScreenReaderOnly>Comment</ScreenReaderOnly>
-    </Button>
+    <CommentDialog postContent={postContent}>
+      <Button variant='ghost' size='icon'>
+        <MessageText className='text-muted-foreground' />
+        <ScreenReaderOnly>Comment</ScreenReaderOnly>
+      </Button>
+    </CommentDialog>
   );
 };
 

229ca1c9cda7296d2cd2177dd8283497358ddbcb
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-14 18:13:33 +0600

    Add user profile and suggestions to home page

--- app/components/home/index.tsx
+++ app/components/home/index.tsx
@@ -1,13 +1,17 @@
 import PostFeed from './post-feed';
 import { Box } from '@/components/layout/box';
 import { Grid } from '@/components/layout/grid';
+import UserProfile from './user-profile';
+import Suggestions from './suggestions';
 
 const Home = () => {
   return (
-    <Grid cols={1} className='container' mt={8} mx='auto' gap={4}>
-      <Box>
+    <Grid cols={4} className='container mx-auto' mt={8} gap={4}>
+      <UserProfile />
+      <Box className='col-span-2'>
         <PostFeed />
       </Box>
+      <Suggestions />
     </Grid>
   );
 };

12c52a0e18ef98ecb42933b5085b03a4b28274e3
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-14 18:11:04 +0600

    Add currentUser filter to useUsers hook

--- hooks/useUsers.ts
+++ hooks/useUsers.ts
@@ -1,15 +1,23 @@
 import useSWR from 'swr';
 import { User } from '@/lib/type';
 import fetcher from '@/lib/fetcher';
+import useCurrentUser from './useCurrentUser';
 
 const useUsers = () => {
   const { data, error, isLoading, mutate } = useSWR<User[]>(
     `/api/users`,
     fetcher
   );
+  const { currentUser } = useCurrentUser();
+
+  const users = data
+    ?.filter((user) => currentUser && user.id !== currentUser.id)
+    .sort((a, b) => {
+      return new Date(a.createdAt).getTime() - new Date(b.createdAt).getTime();
+    });
 
   return {
-    data,
+    users,
     error,
     isLoading,
     mutate,

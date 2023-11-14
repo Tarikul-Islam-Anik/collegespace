287c6971452c7022eaee71008b8a2b9375e35994
MD. Tarikul Islam <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-14 18:13:11 +0600

    Refactor post item component to use separate post content component

--- app/components/home/post-feed/posts/post-item/index.tsx
+++ app/components/home/post-feed/posts/post-item/index.tsx
@@ -1,58 +1,33 @@
-import { useMemo } from 'react';
-import dynamic from 'next/dynamic';
-import { formatDistanceToNowStrict } from 'date-fns';
 import { PostType } from '@/lib/type';
-import PostActions from './item-actions';
-import PostOptions from './item-options';
-import { Card, CardContent, CardFooter } from '@/components/ui/card';
-import UserAvatar from '@/components/shared/user-avatar';
+import PostContent from './post-content';
 import { Flex } from '@/components/layout/flex';
 import { Text } from '@/components/typography/text';
+import ToolTipParent from '@/components/shared/tooltip-parent';
+import { Card, CardContent, CardFooter } from '@/components/ui/card';
+import { HandleLike, HandleComment, HandleRepost } from './item-actions';
 
-const ProfileHoverCard = dynamic(
-  () => import('@/components/shared/profile-hover-card'),
-  { ssr: false }
-);
-
-const PostItem = ({ id, type, content, user, likes, createdAt }: PostType) => {
-  const postedAt = useMemo(() => {
-    return formatDistanceToNowStrict(new Date(createdAt), {
-      addSuffix: true,
-    });
-  }, [createdAt]);
-
+const PostItem = ({ post }: { post: PostType }) => {
+  const postContent = <PostContent post={post} />;
   return (
     <Card className='rounded-none border-none pt-5 shadow-none'>
-      <CardContent className='pb-2 pl-0'>
-        <Flex className='gap-3'>
-          <UserAvatar name={user?.name} image={user?.image} />
-          <Flex direction='column' className='-mt-3' width='full'>
-            <Flex align='center' justify='between'>
-              <Text as='p'>
-                <ProfileHoverCard {...user}>
-                  <Text className='font-semibold'>{user?.name}</Text>
-                </ProfileHoverCard>
-                <Text className='mx-2'>&middot;</Text>
-                <time
-                  dateTime={new Date(createdAt).toISOString()}
-                  className='text-xs'
-                >
-                  {postedAt}
-                </time>
-              </Text>
-              <PostOptions postId={id} />
-            </Flex>
-            <Text as='p'>{content}</Text>
-          </Flex>
-        </Flex>
-      </CardContent>
+      <CardContent className='pb-2 pl-0'>{postContent}</CardContent>
       <CardFooter className='border-b pb-4 pl-14'>
         <Flex align='center' justify='between' width='full'>
           <Text as='p' className='text-muted-foreground' size='xs'>
-            {likes.length} like{likes.length > 1 && 's'}
+            {post.likes.length} like{post.likes.length > 1 && 's'}
             &middot; 2 comments
           </Text>
-          <PostActions postId={id} />
+          <Flex>
+            <ToolTipParent content='Like'>
+              <HandleLike postId={post.id} />
+            </ToolTipParent>
+            <ToolTipParent content='Repost'>
+              <HandleRepost />
+            </ToolTipParent>
+            <ToolTipParent content='Comment'>
+              <HandleComment postContent={postContent} />
+            </ToolTipParent>
+          </Flex>
         </Flex>
       </CardFooter>
     </Card>

72dc8998486b3fd150c43f67e2dfbfaf6108d1d3
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-16 13:25:37 +0600

    Refactor ProfileHoverCard component to use forwardRef

--- components/shared/profile-hover-card.tsx
+++ components/shared/profile-hover-card.tsx
@@ -1,7 +1,8 @@
+import { forwardRef } from 'react';
 import Link from 'next/link';
 import { format } from 'date-fns';
 import { cn } from '@/lib/utils';
-import { User } from '@/lib/type';
+import { User, UserType } from '@/lib/type';
 import { Calendar } from 'iconsax-react';
 import { Button, buttonVariants } from '@/components/ui/button';
 import {
@@ -15,79 +16,87 @@ import { Text } from '@/components/typography/text';
 import UserAvatar from '@/components/shared/user-avatar';
 import { Heading } from '@/components/typography/heading';
 
-interface ProfileHoverCardProps extends Partial<User> {
+interface ProfileHoverCardProps extends Partial<UserType> {
   children: React.ReactNode;
   className?: string;
 }
 
-const ProfileHoverCard = ({
-  id,
-  name,
-  image,
-  bio,
-  username,
-  createdAt,
-  children,
-  className,
-}: ProfileHoverCardProps) => {
-  return (
-    <HoverCard openDelay={200} closeDelay={200}>
-      <HoverCardTrigger asChild>
-        <Button variant='link' className='h-7 justify-start p-0 text-inherit'>
-          {children}
-        </Button>
-      </HoverCardTrigger>
-      <HoverCardContent className={cn(className, 'w-80')}>
-        <Flex direction='column' gap={4}>
-          <Flex gap={4} justify='between'>
-            <UserAvatar name={name} image={image} />
-            <Box className='min-w-[230px] space-y-1'>
-              <Flex className='gap-1.5' mb={2} align='center'>
-                <Heading as='h4' size='sm' weight='semibold'>
-                  {name}
-                </Heading>
-                {username && (
-                  <Text
-                    className={cn(
-                      buttonVariants({
-                        variant: 'secondary',
-                        size: 'sm',
-                      }),
-                      'h-6 rounded px-1'
-                    )}
-                    size='xs'
-                  >{`@${username}`}</Text>
-                )}
-              </Flex>
-              <Text
-                size='xs'
-                className={cn(
-                  !bio && 'italic text-muted-foreground',
-                  'line-clamp-3'
-                )}
-              >
-                {bio ? bio : 'No bio provided'}
-              </Text>
-              <Flex align='center' pt={2}>
-                <Calendar className='mr-2 h-4 w-4 opacity-70' />{' '}
-                <Text size='xs' color='muted-foreground'>
-                  Joined{' '}
-                  {createdAt && format(new Date(createdAt), 'dd, MMMM yyyy')}
+const ProfileHoverCard = forwardRef(
+  (
+    {
+      children,
+      name,
+      username,
+      bio,
+      image,
+      createdAt,
+      id,
+      className,
+    }: ProfileHoverCardProps,
+    ref
+  ) => {
+    return (
+      <HoverCard openDelay={200} closeDelay={200}>
+        <HoverCardTrigger
+          asChild
+          ref={ref as React.RefObject<HTMLAnchorElement>}
+        >
+          <Button variant='link' className='h-7 justify-start p-0 text-inherit'>
+            {children}
+          </Button>
+        </HoverCardTrigger>
+        <HoverCardContent className={cn(className, 'w-80')}>
+          <Flex direction='column' gap={4}>
+            <Flex gap={4} justify='between'>
+              <UserAvatar name={name} image={image} />
+              <Box className='min-w-[230px] space-y-1'>
+                <Flex className='gap-1.5' mb={2} align='center'>
+                  <Heading as='h4' size='sm' weight='semibold'>
+                    {name}
+                  </Heading>
+                  {username && (
+                    <Text
+                      className={cn(
+                        buttonVariants({
+                          variant: 'secondary',
+                          size: 'sm',
+                        }),
+                        'h-6 rounded px-1'
+                      )}
+                      size='xs'
+                    >{`@${username}`}</Text>
+                  )}
+                </Flex>
+                <Text
+                  size='xs'
+                  className={cn(
+                    !bio && 'italic text-muted-foreground',
+                    'line-clamp-3'
+                  )}
+                >
+                  {bio ? bio : 'No bio provided'}
                 </Text>
-              </Flex>
-            </Box>
+                <Flex align='center' pt={2}>
+                  <Calendar className='mr-2 h-4 w-4 opacity-70' />{' '}
+                  <Text size='xs' color='muted-foreground'>
+                    Joined{' '}
+                    {createdAt && format(new Date(createdAt), 'dd, MMMM yyyy')}
+                  </Text>
+                </Flex>
+              </Box>
+            </Flex>
+            <Link
+              href={`/user/${id}`}
+              className={cn(buttonVariants({ variant: 'default' }), 'w-full')}
+            >
+              View profile
+            </Link>
           </Flex>
-          <Link
-            href={`/user/${id}`}
-            className={cn(buttonVariants({ variant: 'default' }), 'w-full')}
-          >
-            View profile
-          </Link>
-        </Flex>
-      </HoverCardContent>
-    </HoverCard>
-  );
-};
+        </HoverCardContent>
+      </HoverCard>
+    );
+  }
+);
 
 ProfileHoverCard.displayName = 'HoverCardDemo';
 export default ProfileHoverCard;

a575e751eb70ecd763d7b2652a5840bfa5fab463
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-16 13:24:20 +0600

    Add, update, and delete like routes for posts.

--- app/api/like/[postId]/route.ts
+++ app/api/like/[postId]/route.ts
@@ -19,3 +19,55 @@ export async function GET(
 
   return NextResponse.json({ liked: !!liked }, { status: 200 });
 }
+
+export async function POST(
+  request: NextRequest,
+  { params }: { params: { postId: string } }
+) {
+  const { currentUser } = await authStatus();
+
+  await prisma.like.create({
+    data: {
+      userId: currentUser.id,
+      postId: params.postId,
+    },
+  });
+
+  const postOwnerId = await prisma.post.findUnique({
+    where: {
+      id: params.postId,
+    },
+    select: {
+      userId: true,
+    },
+  });
+
+  if (postOwnerId?.userId !== currentUser.id) {
+    await prisma.notification.create({
+      data: {
+        userId: postOwnerId!.userId,
+        content: `${currentUser.name} liked your post`,
+      },
+    });
+  }
+
+  return NextResponse.json({ liked: true }, { status: 200 });
+}
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { postId: string } }
+) {
+  const { currentUser } = await authStatus();
+
+  await prisma.like.delete({
+    where: {
+      userId_postId: {
+        postId: params.postId,
+        userId: currentUser.id,
+      },
+    },
+  });
+
+  return NextResponse.json({ liked: false }, { status: 200 });
+}

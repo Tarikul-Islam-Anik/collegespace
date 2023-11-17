9da7334ca586c267bc6d6a548092468e64d48abe
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 00:16:46 +0600

    Add profile page with profile data and activities components

---
+++ app/profile/[email]/page.tsx
@@ -0,0 +1,21 @@
+import { Flex } from '@/components/layout/flex';
+import { Container } from '@/components/layout/container';
+import ProfileData from '../components/profile-data';
+import ProfileActivities from '../components/profile-activites';
+import { Box } from '@/components/layout/box';
+
+const ProfilePage = ({ params }: { params: { email: string } }) => {
+  const { email } = params;
+  return (
+    <Box mx='auto' className='w-[500px]'>
+      <Container>
+        <Flex direction='column' align='start' gap={4}>
+          <ProfileData email={email} />
+          <ProfileActivities email={email} />
+        </Flex>
+      </Container>
+    </Box>
+  );
+};
+
+export default ProfilePage;

f5f56bb57e075352d273ccb2275aaf64554c4f1a
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 00:13:08 +0600

    Add reply route for creating comments and notifications

---
+++ app/api/reply/route.ts
@@ -0,0 +1,36 @@
+import authStatus from '@/lib/auth-status';
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function POST(request: NextRequest) {
+  const { currentUser } = await authStatus();
+  const body = await request.json();
+
+  await prisma.reply.create({
+    data: {
+      content: body.content,
+      userId: currentUser.id,
+      postId: body.id,
+    },
+  });
+
+  const postOwnerId = await prisma.post.findUnique({
+    where: {
+      id: body.id,
+    },
+    select: {
+      userId: true,
+    },
+  });
+
+  if (postOwnerId?.userId !== currentUser.id) {
+    await prisma.notification.create({
+      data: {
+        userId: postOwnerId!.userId,
+        content: `${currentUser.name} commented your post`,
+      },
+    });
+  }
+
+  return NextResponse.json({}, { status: 200 });
+}

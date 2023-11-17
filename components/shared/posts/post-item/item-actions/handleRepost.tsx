0dba5b62570988fbf976eb75c7fd6b6238c1d35a
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 00:08:24 +0600

    Add repost functionality to post item actions

--- components/shared/posts/post-item/item-actions/handleRepost.tsx
+++ components/shared/posts/post-item/item-actions/handleRepost.tsx
@@ -2,11 +2,34 @@ import { Repeat } from 'iconsax-react';
 import { Button } from '@/components/ui/button';
 import ScreenReaderOnly from '@/components/ui/screen-reader-only';
 import ToolTipParent from '@/components/shared/tooltip-parent';
+import { toast } from 'sonner';
 
-const HandleRepost = () => {
+const HandleRepost = ({ postId }: { postId: string }) => {
+  function handleRepost() {
+    toast.promise(
+      fetch(`/api/posts/repost`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          postId,
+        }),
+      }),
+      {
+        loading: 'Reposting...',
+        success: (res) => {
+          if (res.status === 201) return 'Reposted!';
+          else return 'You have already reposted this post!';
+        },
+        error: 'Something went wrong!',
+      }
+    );
+  }
+
   return (
     <ToolTipParent content='Repost'>
-      <Button variant='ghost' size='icon'>
+      <Button variant='ghost' size='icon' onClick={handleRepost}>
         <Repeat className='text-muted-foreground' />
         <ScreenReaderOnly>Repost</ScreenReaderOnly>
       </Button>

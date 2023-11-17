2f7e6fa6382414b9ed70ee1b8512ef2d67a0a92c
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 00:18:34 +0600

    Add user suggestions and site footer to home page

---
+++ app/components/home/suggestions/index.tsx
@@ -0,0 +1,16 @@
+
+import { Flex } from '@/components/layout/flex';
+import UserSuggestions from './user-suggestions.tsx';
+import SiteFooter from '@/app/components/layout/site-footer';
+
+const Suggestions = () => {
+  return (
+    <Flex direction='column' align='center' gap={4} width='full'>
+      <UserSuggestions />
+      <SiteFooter show />
+    </Flex>
+  );
+};
+
+Suggestions.displayName = 'Suggestions';
+export default Suggestions;

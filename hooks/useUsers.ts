cd1f97c76d7e649444be95529b53cfabf331aeb9
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 00:53:51 +0600

    Refactor useUsers hook to filter users based on current user's followers

--- hooks/useUsers.ts
+++ hooks/useUsers.ts
@@ -1,16 +1,26 @@
 import useSWR from 'swr';
-import { User } from '@/lib/type';
+import { UserType } from '@/lib/type';
 import fetcher from '@/lib/fetcher';
 import { useSession } from 'next-auth/react';
+import useFollower from './useFollower';
 
 const useUsers = () => {
   const { data: session } = useSession();
-  const { data, error, isLoading, mutate } = useSWR<User[]>(
+  const { data, error, isLoading, mutate } = useSWR<UserType[]>(
     `/api/users`,
     fetcher
   );
+
+  // @ts-ignore
+  const currentUserId = session?.user?.id;
+
+  const { data: following } = useFollower(currentUserId);
+
   const users = data?.filter(
-    (user) => session && user.email !== session?.user?.email
+    (user) =>
+      session &&
+      user.email !== session?.user?.email &&
+      following?.follows?.some((follow) => follow.id === currentUserId)
   );
 
   return {

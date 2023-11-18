d284f6e6263f269b519307c4a003815c63408dc7
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 18:01:42 +0600

    Add route for fetching student details by email

---
+++ app/api/student-details/[email]/route.ts
@@ -0,0 +1,27 @@
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+
+export async function GET(
+  request: NextRequest,
+  { params }: { params: { email: string } }
+) {
+  const user = await prisma.user.findUnique({
+    where: { email: params.email },
+    select: {
+      name: true,
+      email: true,
+      phone: true,
+      studentDetails: {
+        include: {
+          educations: true,
+          projects: true,
+        },
+      },
+    },
+  });
+
+  if (!user)
+    return NextResponse.json({ error: 'User not found' }, { status: 404 });
+
+  return NextResponse.json(user);
+}

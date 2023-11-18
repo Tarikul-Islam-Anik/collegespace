2832fd618801d2804c27f0b3bd26b9454f1034a0
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 23:03:33 +0600

    Add post page component with replies

---
+++ app/post/[postId]/page.tsx
@@ -0,0 +1,38 @@
+'use client';
+
+import { ReplyType } from '@/lib/type';
+import usePost from '@/hooks/usePost';
+import { Flex } from '@/components/layout/flex';
+import Loader from '@/components/shared/loader';
+import { Separator } from '@/components/ui/separator';
+import { Container } from '@/components/layout/container';
+import PostItem from '@/components/shared/posts/post-item';
+import ReplyItem from '@/components/shared/posts/reply/reply-item';
+
+export default function Page({ params }: { params: { postId: string } }) {
+  const { data: post, isLoading } = usePost(params.postId);
+  const replies = post ? post?.replies : [];
+
+  if (isLoading) return <Loader className='h-96' />;
+
+  return (
+    <Container className='w-[500px]'>
+      <Flex direction='column'>
+        {post && <PostItem post={post} />}
+        <ul role='reply-list'>
+          {replies.map((reply: ReplyType, index) => (
+            <li key={reply.id}>
+              {index !== 0 && <Separator />}
+              <ReplyItem
+                id={reply.id}
+                content={reply.content}
+                createdAt={reply.createdAt}
+                user={reply.user}
+              />
+            </li>
+          ))}
+        </ul>
+      </Flex>
+    </Container>
+  );
+}

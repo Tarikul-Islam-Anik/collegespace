eece966682b5755a89337bb4a3f5567b5130f8ea
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 12:23:06 +0600

    Update CompanyForm import and add dynamic loading

--- app/settings/company/page.tsx
+++ app/settings/company/page.tsx
@@ -1,6 +1,11 @@
-import CompanyForm from './company-form';
+import dynamic from 'next/dynamic';
 import { Box } from '@/components/layout/box';
+import Loader from '@/components/shared/loader';
 import SectionHeading from '../components/section-heading';
+
+const CompanyForm = dynamic(() => import('./company-form'), {
+  loading: () => <Loader />,
+});
 
 export default function SettingsCompanyPage() {
   return (

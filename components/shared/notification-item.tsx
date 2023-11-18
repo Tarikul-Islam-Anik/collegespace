41b82efce93ad75449b8ca80fcf52829dc13d6e7
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-18 23:03:27 +0600

    Add NotificationItem component

---
+++ components/shared/notification-item.tsx
@@ -0,0 +1,23 @@
+import { Box } from '@/components/layout/box';
+import { Button } from '@/components/ui/button';
+import { Flex } from '@/components/layout/flex';
+import { Text } from '@/components/typography/text';
+
+interface NotificationType {
+  content: string;
+}
+
+const NotificationItem = ({ content }: NotificationType) => {
+  return (
+    <Flex align='center' justify='between'>
+      <Text>{content}</Text>
+      {content.includes('following') && (
+        <Button size='sm' variant='outline'>
+          Follow back
+        </Button>
+      )}
+    </Flex>
+  );
+};
+
+export default NotificationItem;

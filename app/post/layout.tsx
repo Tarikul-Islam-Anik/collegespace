477f8986d97a7afe96f7edd8d6a8117e7105425c
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-19 15:35:07 +0600

    Add post layout component

---
+++ app/post/layout.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from 'next';
+import { siteConfig } from '@/config/site';
+import { Box } from '@/components/layout/box';
+import BackButton from './components/back-button';
+import { Container } from '@/components/layout/container';
+
+export const metadata: Metadata = {
+  title: 'Post | ' + siteConfig.name,
+  description: 'Post page.',
+};
+
+interface PostLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function PostLayout({ children }: PostLayoutProps) {
+  return (
+    <>
+      <Box mx='auto' className='page-width' position='relative'>
+        <Box className='fixed left-4 top-3.5 sm:absolute sm:-left-10 sm:top-5'>
+          <BackButton />
+        </Box>
+        <Container>{children}</Container>
+      </Box>
+    </>
+  );
+}

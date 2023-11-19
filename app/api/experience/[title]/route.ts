b9b52ba0c2704df3533d63f7cd225470432434ae
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-20 00:44:02 +0600

    Add DELETE route for experience

---
+++ app/api/experience/[title]/route.ts
@@ -0,0 +1,20 @@
+import prisma from '@/lib/prisma';
+import { NextRequest, NextResponse } from 'next/server';
+import authStatus from '@/lib/auth-status';
+
+export async function DELETE(
+  request: NextRequest,
+  { params }: { params: { title: string } }
+) {
+  const { currentUser } = await authStatus();
+  await prisma.experience.delete({
+    where: {
+      studentId_title: {
+        studentId: currentUser.id,
+        title: params.title,
+      },
+    },
+  });
+
+  return NextResponse.json({ success: true }, { status: 200 });
+}

7a9ec10ec16e5e7a39bbbad3651107bdf8fc5069
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-20 00:45:12 +0600

    Add experience form schema

---
+++ app/settings/experience/experience-form/schema.ts
@@ -0,0 +1,11 @@
+import z from 'zod';
+
+export const experienceFormSchema = z.object({
+  title: z.string().max(101),
+  description: z.string(),
+  position: z.string().max(51),
+  startDate: z.date(),
+  endDate: z.date(),
+});
+
+export type ExperienceFormValue = z.infer<typeof experienceFormSchema>;

7b2a41274a57738e011dd2b2427353a1cba03382
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-20 00:43:06 +0600

    Refactor profile activities tab names

--- app/profile/components/profile-activites/index.tsx
+++ app/profile/components/profile-activites/index.tsx
@@ -25,13 +25,13 @@ const ProfileActivities = ({ email }: { email: string }) => {
       className='mx-auto flex w-full flex-col items-center'
     >
       <TabsList className='w-full justify-around rounded-none border-b bg-transparent p-0'>
-        {['recent', 'replies', 'reposts'].map((tab) => (
+        {tabsContent.map((tab) => (
           <TabsTrigger
-            key={tab}
-            value={tab}
-            className='w-full rounded-none pb-2 data-[state=active]:border-b-4 data-[state=active]:bg-transparent data-[state=active]:shadow-none'
+            key={tab.name}
+            value={tab.name}
+            className='w-full rounded-none pb-2 capitalize data-[state=active]:border-b-4 data-[state=active]:bg-transparent data-[state=active]:shadow-none'
           >
-            {tab[0].toUpperCase() + tab.slice(1)}
+            {tab.name}
           </TabsTrigger>
         ))}
       </TabsList>

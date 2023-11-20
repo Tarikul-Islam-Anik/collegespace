5d46dd7875c28ef3cccfdfeb78f68177d6c52123
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-20 14:31:25 +0600

    Add CV layout component

---
+++ app/cv/layout.tsx
@@ -0,0 +1,27 @@
+import { Metadata } from 'next';
+import { siteConfig } from '@/config/site';
+import { Box } from '@/components/layout/box';
+import BackButton from '@/components/shared/back-button';
+import { Container } from '@/components/layout/container';
+
+export const metadata: Metadata = {
+  title: 'CV | ' + siteConfig.name,
+  description: "Student's CV page.",
+};
+
+interface PostLayoutProps {
+  children: React.ReactNode;
+}
+
+export default function PostLayout({ children }: PostLayoutProps) {
+  return (
+    <>
+      <Box mx='auto' position='relative'>
+        <Box className='fixed left-4 top-3.5 z-50 sm:absolute sm:left-[32.5rem] sm:top-0'>
+          <BackButton />
+        </Box>
+        <Container>{children}</Container>
+      </Box>
+    </>
+  );
+}

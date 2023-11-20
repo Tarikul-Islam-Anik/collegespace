ce5ed13d9f802ee4b4b956234167e17d688d4964
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-20 14:31:38 +0600

    Add StudentAboutInfoItem component

---
+++ app/cv/components/student-about-item.tsx
@@ -0,0 +1,81 @@
+import Link from 'next/link';
+import { Box } from '@/components/layout/box';
+import { Flex } from '@/components/layout/flex';
+import { Heading } from '@/components/typography/heading';
+import { Text } from '@/components/typography/text';
+import { buttonVariants } from '@/components/ui/button';
+import YearRange from '@/components/shared/year-range';
+import { GraduationCap } from 'lucide-react';
+import { Badge } from '@/components/ui/badge';
+
+interface StudentInfoItemProps {
+  title: string;
+  description?: string;
+  school?: string;
+  link?: string | null;
+  jobType?:
+    | 'internship'
+    | 'full-time'
+    | 'part-time'
+    | 'contract'
+    | 'freelance'
+    | 'volunteer'
+    | 'remote';
+  startDate: Date;
+  endDate: Date;
+}
+
+const StudentAboutInfoItem = ({
+  title,
+  description,
+  school,
+  link,
+  jobType,
+  startDate,
+  endDate,
+}: StudentInfoItemProps) => {
+  return (
+    <Box>
+      <Heading as='h4' className='mb-1.5' weight='medium'>
+        {title}
+      </Heading>
+      <Text as='p' size='sm' color='muted-foreground'>
+        {description}
+      </Text>
+      <Flex
+        align='center'
+        justify='between'
+        mt={4}
+        className='text-muted-foreground'
+      >
+        <Flex align='center' className='gap-1.5'>
+          {link && (
+            <Link
+              href={link}
+              className={buttonVariants({ variant: 'outline', size: 'sm' })}
+            >
+              View Project
+            </Link>
+          )}
+          {school && (
+            <>
+              <GraduationCap size='16' />
+              <Text as='p' size='sm'>
+                University of California
+              </Text>
+            </>
+          )}
+          {jobType && (
+            <Badge variant='outline' className='capitalize'>
+              {jobType}
+            </Badge>
+          )}
+        </Flex>
+        <YearRange startDate={startDate} endDate={endDate} />
+      </Flex>
+    </Box>
+  );
+};
+
+StudentAboutInfoItem.displayName = 'StudentAboutInfoItem';
+export default StudentAboutInfoItem;

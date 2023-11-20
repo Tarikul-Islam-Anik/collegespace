71766b48680868ba59dd827fe9ea53037c76dd72
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-21 00:09:04 +0600

    Add button to view CV in notification item

--- components/shared/notification-item.tsx
+++ components/shared/notification-item.tsx
@@ -15,6 +15,11 @@ const NotificationItem = ({ content }: NotificationType) => {
           Follow back
         </Button>
       )}
+      {content.includes('job') && (
+        <Button size='sm' variant='outline'>
+          View CV
+        </Button>
+      )}
     </Flex>
   );
 };

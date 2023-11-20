d7fcd24c1aa66f6171613cb8b079a1788ff2f07a
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-21 00:11:10 +0600

    Add RemoveJobItem component to shared job list

---
+++ components/shared/job-list/remove-job-item.tsx
@@ -0,0 +1,43 @@
+import { toast } from 'sonner';
+import useJobs from '@/hooks/useJobs';
+import { Button } from '@/components/ui/button';
+import AlertDialogParent from '../alert-dialog-parent';
+
+const RemoveJobItem = ({
+  jobId,
+  companyId,
+}: {
+  jobId: string;
+  companyId: string;
+}) => {
+  const { mutate } = useJobs(companyId);
+  function handleDelete() {
+    toast.promise(
+      fetch(`/api/jobs/${jobId}`, {
+        method: 'DELETE',
+      }),
+      {
+        loading: 'Deleting...',
+        success: () => {
+          mutate();
+          return 'Job deleted';
+        },
+        error: 'Error deleting job',
+      }
+    );
+  }
+  return (
+    <AlertDialogParent
+      title='Delete Job post'
+      description='Are you sure you want to delete this job? This action cannot be undone. All applicants data will be removed.'
+      action={handleDelete}
+    >
+      <Button size='sm' variant='outline'>
+        Remove
+      </Button>
+    </AlertDialogParent>
+  );
+};
+
+RemoveJobItem.displayName = 'RemoveJobItem';
+export default RemoveJobItem;

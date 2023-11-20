6e189cd082f023a21d39c6a9d228bf4b4f51cf15
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-20 14:21:58 +0600

    Update description character limit

--- app/settings/projects/project-form/schema.ts
+++ app/settings/projects/project-form/schema.ts
@@ -2,7 +2,7 @@ import z from 'zod';
 
 export const projectFormSchema = z.object({
   title: z.string().max(101),
-  description: z.string().max(500),
+  description: z.string().max(1000),
   website: z.string().max(100),
   startDate: z.date(),
   endDate: z.date(),

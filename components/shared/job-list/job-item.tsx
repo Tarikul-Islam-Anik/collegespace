0ec79c7a44e465c5244fccbe57c0fc3667996527
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-21 00:11:05 +0600

    Add JobItem component to shared job list

---
+++ components/shared/job-list/job-item.tsx
@@ -0,0 +1,69 @@
+'use client';
+import { Location } from 'iconsax-react';
+import { JobType } from '@/lib/type';
+import useCurrentUser from '@/hooks/useCurrentUser';
+import { Flex } from '@/components/layout/flex';
+import { Text } from '@/components/typography/text';
+import { Heading } from '@/components/typography/heading';
+import { Button } from '@/components/ui/button';
+import RemoveJobItem from './remove-job-item';
+import ViewJobItem from './view-job-item';
+import CompanyDetails from '../company-details';
+
+export const jobType = (type: string) => {
+  switch (type) {
+    case 'fulltime':
+      return 'Full Time';
+    case 'parttime':
+      return 'Part Time';
+    default:
+      return type;
+  }
+};
+
+const JobItem = ({ job }: { job: JobType }) => {
+  const { currentUser } = useCurrentUser();
+  const isOwner =
+    currentUser?.company && currentUser?.company.id === job.companyId;
+
+  return (
+    <Flex justify='between' align={'center'}>
+      <Flex direction='column' gap={2}>
+        <Heading as='h3' weight='medium'>
+          {job.title}
+        </Heading>
+        <Text
+          as='p'
+          color='muted-foreground'
+          size='sm'
+          className='line-clamp-3'
+        >
+          {job.description}
+        </Text>
+        <Flex gap={2} align='center' className='text-sm text-muted-foreground'>
+          <Flex align='center'>
+            <Location size={12} className='mr-1' />
+            <Text as='p'>{job.location}</Text>
+          </Flex>
+          <Text size='xs'>&bull;</Text>
+          <Text className='capitalize'>{jobType(job.type)}</Text>
+          <Text size='xs'>&bull;</Text>
+          <CompanyDetails name={job.company.name} id={job.companyId} />
+        </Flex>
+      </Flex>
+      {isOwner ? (
+        <Flex gap={2}>
+          <Button size='sm' variant='outline'>
+            View applicants
+          </Button>
+          <RemoveJobItem jobId={job.id} companyId={job.companyId} />
+        </Flex>
+      ) : (
+        <ViewJobItem job={job} />
+      )}
+    </Flex>
+  );
+};
+
+JobItem.displayName = 'JobItem';
+export default JobItem;

37fbff307899205fee97811272588f2a112b2c1b
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-23 18:57:16 +0600

    Remove unused files and dependencies

--- lib/atom.ts
+++
@@ -1,8 +0,0 @@
-import { atom } from 'jotai';
-import { atomWithStorage } from 'jotai/utils';
-import { UserType, PostType } from './type';
-
-const UserAtom = atomWithStorage<UserType | null>('user', null);
-const PostsAtom = atom<PostType[] | null>(null);
-
-export { UserAtom, PostsAtom };

52e73674f2d4fe9dcbf73d30d72e4086fe11831e
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-23 20:26:32 +0600

    Update API endpoint for fetching company jobs

--- hooks/useJobs.ts
+++ hooks/useJobs.ts
@@ -4,7 +4,7 @@ import fetcher from '@/lib/fetcher';
 
 const useJobs = (companyId?: string) => {
   const { data, error, isLoading, mutate } = useSWR<{ jobs: JobType[] }>(
-    companyId ? `/api/jobs/${companyId}` : '/api/jobs',
+    companyId ? `/api/company/jobs/${companyId}` : '/api/company/jobs',
     fetcher
   );
 

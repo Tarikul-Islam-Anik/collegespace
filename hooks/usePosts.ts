4614d68bd2f8a8873986ae19b4961b2c3009514d
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-23 23:06:20 +0600

    Refactor usePosts hook to handle different post types

--- hooks/usePosts.ts
+++ hooks/usePosts.ts
@@ -2,17 +2,28 @@ import useSWR from 'swr';
 import fetcher from '@/lib/fetcher';
 import { PostType } from '@/lib/type';
 
-const usePosts = () => {
-  const endpoint = '/api/posts';
-  const {
-    data: posts,
-    error,
-    isLoading,
-    mutate,
-  } = useSWR<PostType[]>(endpoint, fetcher);
+const usePosts = (
+  reference?: string,
+  postType: 'recent' | 'replies' | 'single' | 'repost' | 'all' = 'all'
+) => {
+  const endpoint =
+    postType === 'single' && reference
+      ? `/api/posts/${reference}`
+      : postType === 'recent' && reference
+      ? `/api/posts/recent/${reference}`
+      : postType === 'replies' && reference
+      ? `/api/posts/reply/${reference}`
+      : postType === 'repost' && reference
+      ? `/api/posts/repost/${reference}`
+      : '/api/posts';
+
+  const { data, error, isLoading, mutate } = useSWR<PostType>(
+    endpoint,
+    fetcher
+  );
 
   return {
-    posts,
+    data,
     error,
     isLoading,
     mutate,

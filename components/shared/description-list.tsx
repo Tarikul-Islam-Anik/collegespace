5dfcfcfbc7f3c3732622f5e2be8c15742a26ae91
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-24 00:25:31 +0600

    Refactor DescriptionList component

--- components/shared/description-list.tsx
+++ components/shared/description-list.tsx
@@ -2,12 +2,14 @@ import { Box } from '../layout/box';
 
 const DescriptionList = ({ name, value }: { name: any; value: any }) => {
   return (
-    <Box className='sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
-      <dt className='text-sm font-medium capitalize leading-6'>{name}</dt>
-      <dd className='mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0'>
-        {value}
-      </dd>
-    </Box>
+    value && (
+      <Box className='sm:grid sm:grid-cols-3 sm:gap-4 sm:px-0'>
+        <dt className='text-sm font-medium capitalize leading-6'>{name}</dt>
+        <dd className='mt-1 text-sm leading-6 text-muted-foreground sm:col-span-2 sm:mt-0'>
+          {value}
+        </dd>
+      </Box>
+    )
   );
 };
 

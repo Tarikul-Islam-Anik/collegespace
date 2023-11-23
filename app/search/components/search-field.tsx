0732facaec3617947b60b45514b4b5211737328b
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-23 18:55:08 +0600

    Add scroll functionality to search results

--- app/search/components/search-field.tsx
+++ app/search/components/search-field.tsx
@@ -11,6 +11,7 @@ import SearchItem from './search-items';
 import { Separator } from '@/components/ui/separator';
 import Loader from '@/components/shared/loader';
 import { Box } from '@/components/layout/box';
+import { ScrollArea } from '@/components/ui/scroll-area';
 
 const SearchField = () => {
   const { users, isLoading } = useUsers();
@@ -21,18 +22,20 @@ const SearchField = () => {
       <CommandList className='max-h-full'>
         <CommandEmpty>No results found.</CommandEmpty>
         {isLoading ? (
-          <Loader className='mt-4'/>
+          <Loader className='mt-4' />
         ) : (
-          <CommandGroup className='mt-4'>
-            {users?.map((user, index) => {
-              return (
-                <Box key={index}>
-                  {index !== 0 && <Separator className='my-4' />}
-                  <SearchItem user={user} />
-                </Box>
-              );
-            })}
-          </CommandGroup>
+          <ScrollArea className='h-[65vh]'>
+            <CommandGroup className='mt-4'>
+              {users?.map((user, index) => {
+                return (
+                  <Box key={index}>
+                    {index !== 0 && <Separator className='my-2' />}
+                    <SearchItem user={user} />
+                  </Box>
+                );
+              })}
+            </CommandGroup>
+          </ScrollArea>
         )}
       </CommandList>
     </Command>

94e5d139559a661b1eae711d1f7a866c0e357f72
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-24 00:06:41 +0600

    Fix import and variable names in SettingsSkillsPage

--- app/settings/experience/page.tsx
+++ app/settings/experience/page.tsx
@@ -1,15 +1,16 @@
 'use client';
 import { useState } from 'react';
 import dynamic from 'next/dynamic';
+import { Star1 } from 'iconsax-react';
 import { Box } from '@/components/layout/box';
 import Loader from '@/components/shared/loader';
 import { Flex } from '@/components/layout/flex';
 import EmptyState from '@/components/shared/empty-state';
 import FormDialog from '@/components/shared/form-dialog';
 import useStudentDetails from '@/hooks/useStudentDetails';
-import SectionHeading from '../../../components/shared/section-heading';
 import ListContainer from '@/components/shared/list-container';
-import { Star1 } from 'iconsax-react';
+import SectionHeading from '@/components/shared/section-heading';
+import MissingInfoWaring from '../components/missing-info-warning';
 
 const ExperienceForm = dynamic(() => import('./experience-form'), {
   ssr: false,
@@ -19,8 +20,8 @@ const ExperienceForm = dynamic(() => import('./experience-form'), {
 export default function SettingsSkillsPage() {
   const [open, setOpen] = useState(false);
   const { data } = useStudentDetails();
-  const experiences = data?.studentDetails?.experiences;
-  const sortedExperiences = experiences?.sort(
+  const experienceInfo = data?.studentDetails?.experiences;
+  const sortedExperiences = experienceInfo?.sort(
     (a, b) => new Date(b.endDate).getTime() - new Date(a.endDate).getTime()
   );
 
@@ -36,26 +37,29 @@ export default function SettingsSkillsPage() {
     </FormDialog>
   );
 
+  const experiences =
+    sortedExperiences?.length === 0 ? (
+      <EmptyState
+        title='No previous experience found'
+        description='Add to your profile and attract more recruiters.'
+        icon={<Star1 className='h-12 w-12' />}
+      >
+        {AddNew}
+      </EmptyState>
+    ) : (
+      sortedExperiences && (
+        <ListContainer type='experience' items={sortedExperiences} />
+      )
+    );
+
   return (
     <Box className='space-y-6'>
       <SectionHeading
         title='Experience'
         description='Share past jobs and internships experiences to your profile. Leave blank if you don’t have any.'
       />
-      {sortedExperiences?.length === 0 ? (
-        <EmptyState
-          title='No previous experience found'
-          description='Add to your profile and attract more recruiters.'
-          icon={<Star1 className='h-12 w-12' />}
-        >
-          {AddNew}
-        </EmptyState>
-      ) : (
-        sortedExperiences && (
-          <ListContainer type='experience' items={sortedExperiences} />
-        )
-      )}
-      {experiences?.length! > 0 && sortedExperiences?.length! < 4 && (
+      {!experienceInfo ? <MissingInfoWaring /> : experiences}
+      {experienceInfo?.length! > 0 && sortedExperiences?.length! < 4 && (
         <Flex justify='center'>{AddNew}</Flex>
       )}
     </Box>

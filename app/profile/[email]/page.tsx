662b60d5f97f033ee0077696a6d4c0e664a7a868
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-23 20:58:22 +0600

    Refactor profile page to include user data and loader.

--- app/profile/[email]/page.tsx
+++ app/profile/[email]/page.tsx
@@ -1,20 +1,30 @@
+'use client';
+
+import useUser from '@/hooks/useUser';
+import { Box } from '@/components/layout/box';
 import { Flex } from '@/components/layout/flex';
 import { Container } from '@/components/layout/container';
 import ProfileData from '../components/profile-data';
 import ProfileActivities from '../components/profile-activites';
-import { Box } from '@/components/layout/box';
+import Loader from '@/components/shared/loader';
 
 const ProfilePage = ({ params }: { params: { email: string } }) => {
   const { email } = params;
-  return (
-    <Box mx='auto' className='page-width'>
-      <Container>
-        <Flex direction='column' align='start' gap={4}>
-          <ProfileData email={email} />
-          <ProfileActivities email={email} />
-        </Flex>
-      </Container>
-    </Box>
+  const { user, isLoading } = useUser(email);
+
+  return isLoading ? (
+    <Loader className='h-[80vh]' />
+  ) : (
+    user && (
+      <Box mx='auto' className='page-width'>
+        <Container>
+          <Flex direction='column' align='start' gap={4}>
+            <ProfileData user={user} isLoading={isLoading} />
+            <ProfileActivities email={email} />
+          </Flex>
+        </Container>
+      </Box>
+    )
   );
 };
 

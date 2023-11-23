47954364de94319916145a3b1af616fa23ab62c8
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-23 19:07:25 +0600

    Delete useCurrentUser hook

--- hooks/useCurrentUser.ts
+++
@@ -1,9 +0,0 @@
-import { useAtom } from 'jotai';
-import { UserAtom } from '@/lib/atom';
-
-const useCurrentUser = () => {
-  const [currentUser, setCurrentUser] = useAtom(UserAtom);
-  return { currentUser, setCurrentUser };
-};
-
-export default useCurrentUser;

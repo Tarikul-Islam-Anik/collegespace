852515575f3d885b68736cbe5ba3485737b63f34
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-24 20:42:29 +0600

    Refactor job item component

--- components/shared/job-list/job-item.tsx
+++ components/shared/job-list/job-item.tsx
@@ -1,14 +1,12 @@
 'use client';
 import { Location } from 'iconsax-react';
 import { JobType } from '@/lib/type';
-import useUser from '@/hooks/useUser';
+import { Box } from '@/components/layout/box';
+import CompanyDetails from '../company-details';
+import JobItemOptions from './job-item-options';
 import { Flex } from '@/components/layout/flex';
 import { Text } from '@/components/typography/text';
 import { Heading } from '@/components/typography/heading';
-import { Button } from '@/components/ui/button';
-import RemoveJobItem from './remove-job-item';
-import ViewJobItem from './view-job-item';
-import CompanyDetails from '../company-details';
 
 export const jobType = (type: string) => {
   switch (type) {
@@ -22,9 +20,6 @@ export const jobType = (type: string) => {
 };
 
 const JobItem = ({ job }: { job: JobType }) => {
-  const { user } = useUser();
-  const isOwner = user?.company && user?.company.id === job.companyId;
-
   return (
     <Flex justify='between' align={'center'}>
       <Flex direction='column' gap={2}>
@@ -48,24 +43,13 @@ const JobItem = ({ job }: { job: JobType }) => {
           </Flex>
           <Text size='xs'>&bull;</Text>
           <Text className='capitalize'>{jobType(job.type)}</Text>
-          {!isOwner && (
-            <>
-              <Text size='xs'>&bull;</Text>
-              <CompanyDetails name={job.company.name} id={job.companyId} />
-            </>
-          )}
+          <Text size='xs'>&bull;</Text>
+          <CompanyDetails name={job.company.name} id={job.companyId} />
         </Flex>
       </Flex>
-      {isOwner ? (
-        <Flex gap={2}>
-          <Button size='sm' variant='outline'>
-            View applicants
-          </Button>
-          <RemoveJobItem jobId={job.id} companyId={job.companyId} />
-        </Flex>
-      ) : (
-        <ViewJobItem job={job} />
-      )}
+      <Box>
+        <JobItemOptions job={job} />
+      </Box>
     </Flex>
   );
 };

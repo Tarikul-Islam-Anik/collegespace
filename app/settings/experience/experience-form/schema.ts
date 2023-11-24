9db523d49063070850a3ff188e33e9cedd76ec84
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-24 14:50:33 +0600

    Add type field to experience form schema.

--- app/settings/experience/experience-form/schema.ts
+++ app/settings/experience/experience-form/schema.ts
@@ -4,6 +4,7 @@ export const experienceFormSchema = z.object({
   title: z.string().max(101),
   description: z.string(),
   position: z.string().max(51),
+  type: z.string().max(51),
   startDate: z.date(),
   endDate: z.date(),
 });

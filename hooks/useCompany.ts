d468ec5a26d53683b252fb2beb44a48006165f67
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-24 20:41:30 +0600

    Refactor useCompany hook to accept an optional id parameter.

--- hooks/useCompany.ts
+++ hooks/useCompany.ts
@@ -2,13 +2,14 @@ import useSWR from 'swr';
 import { CompanyType } from '@/lib/type';
 import fetcher from '@/lib/fetcher';
 
-const useCompany = (id: string) => {
+const useCompany = (id?: string) => {
+  const endpoint = 'api/company' + (id ? '/' + id : '');
   const {
     data: company,
     error,
     isLoading,
     mutate,
-  } = useSWR<CompanyType>('api/company/' + id, fetcher);
+  } = useSWR<CompanyType>(endpoint, fetcher);
 
   return {
     company,

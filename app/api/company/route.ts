3ebb65b2ce1badfcd9f7d167235dbd6b78529068
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-24 20:41:49 +0600

    Add GET route to fetch company details

--- app/api/company/route.ts
+++ app/api/company/route.ts
@@ -2,6 +2,21 @@ import { NextRequest, NextResponse } from 'next/server';
 import prisma from '@/lib/prisma';
 import authStatus from '@/lib/auth-status';
 
+export async function GET(request: NextRequest) {
+  const { currentUser } = await authStatus();
+
+  const company = await prisma.company.findUnique({
+    where: {
+      ownerId: currentUser.id,
+    },
+    include: {
+      jobs: true,
+    },
+  });
+
+  return NextResponse.json(company, { status: 200 });
+}
+
 export async function PATCH(request: NextRequest) {
   const body = await request.json();
   const { currentUser } = await authStatus();

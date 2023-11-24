5f7ed59d455376c0527ca9d3a3cdf6b176fc2835
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-24 20:42:20 +0600

    Refactor ViewJobItem component

--- components/shared/job-list/view-job-item.tsx
+++ components/shared/job-list/view-job-item.tsx
@@ -1,24 +1,22 @@
 'use client';
 
+import { toast } from 'sonner';
 import { useState } from 'react';
+import { format } from 'date-fns';
 import { Job } from '@/lib/type';
 import { Button } from '@/components/ui/button';
 import {
-  Dialog,
   DialogContent,
   DialogFooter,
   DialogHeader,
   DialogTitle,
-  DialogTrigger,
 } from '@/components/ui/dialog';
 import { Box } from '@/components/layout/box';
-import { format } from 'date-fns';
 import { jobType } from './job-item';
 import { Separator } from '@/components/ui/separator';
-import { toast } from 'sonner';
 import DescriptionList from '../description-list';
 
-const ViewJobItem = ({ job }: { job: Job }) => {
+const ViewJobItem = ({ job, showApply }: { job: Job; showApply?: boolean }) => {
   const details: { [key: string]: any } = {};
 
   if (job) {
@@ -54,52 +52,47 @@ const ViewJobItem = ({ job }: { job: Job }) => {
     );
   }
   return (
-    <Dialog open={open} onOpenChange={setOpen}>
-      <DialogTrigger asChild>
-        <Button size='sm' variant='outline'>
-          View
-        </Button>
-      </DialogTrigger>
-      <DialogContent className='gap-0'>
-        <DialogHeader>
-          <DialogTitle>{job.title}</DialogTitle>
-        </DialogHeader>
-        <Box className='mt-6'>
-          <dl>
-            {Object.entries(details).map(
-              ([key, value], index) =>
-                !(value instanceof Date) && (
-                  <>
-                    <DescriptionList
-                      name={key}
-                      value={
-                        key === 'deadline'
-                          ? format(new Date(value!), 'dd MMMM yyyy')
-                          : key === 'salary'
-                          ? `${new Intl.NumberFormat('en-US', {
-                              style: 'currency',
-                              currency: 'BDT',
-                              minimumFractionDigits: 0,
-                              currencyDisplay: 'narrowSymbol',
-                            }).format(value as number)}`
-                          : jobType(value as string)
-                      }
-                    />
-                    {index !== Object.entries(details).length - 1 && (
-                      <Separator className='my-4 sm:my-6' />
-                    )}
-                  </>
-                )
-            )}
-          </dl>
-        </Box>
+    <DialogContent className='gap-0'>
+      <DialogHeader>
+        <DialogTitle>{job.title}</DialogTitle>
+      </DialogHeader>
+      <Box className='mt-6'>
+        <dl>
+          {Object.entries(details).map(
+            ([key, value], index) =>
+              !(value instanceof Date) && (
+                <>
+                  <DescriptionList
+                    name={key}
+                    value={
+                      key === 'deadline'
+                        ? format(new Date(value!), 'dd MMMM yyyy')
+                        : key === 'salary'
+                        ? `${new Intl.NumberFormat('en-US', {
+                            style: 'currency',
+                            currency: 'BDT',
+                            minimumFractionDigits: 0,
+                            currencyDisplay: 'narrowSymbol',
+                          }).format(value as number)}`
+                        : jobType(value as string)
+                    }
+                  />
+                  {index !== Object.entries(details).length - 1 && (
+                    <Separator className='my-4 sm:my-6' />
+                  )}
+                </>
+              )
+          )}
+        </dl>
+      </Box>
+      {showApply && (
         <DialogFooter className='mt-8'>
           <Button type='submit' className='w-full' onClick={handleApply}>
             Apply
           </Button>
         </DialogFooter>
-      </DialogContent>
-    </Dialog>
+      )}
+    </DialogContent>
   );
 };
 

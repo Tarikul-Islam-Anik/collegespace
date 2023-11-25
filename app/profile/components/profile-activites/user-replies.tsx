d16434aa9c7c65bce52d37f7229f7831f26ea378
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-25 12:41:17 +0600

    Add RepliedPostItem component and Separator component to UserReplies

--- app/profile/components/profile-activites/user-replies.tsx
+++ app/profile/components/profile-activites/user-replies.tsx
@@ -5,9 +5,13 @@ import usePosts from '@/hooks/usePosts';
 import { Messages3 } from 'iconsax-react';
 import Loader from '@/components/shared/loader';
 import EmptyState from '@/components/shared/empty-state';
+import { Separator } from '@/components/ui/separator';
+import useUser from '@/hooks/useUser';
+import RepliedPostItem from '@/components/shared/posts/replied-post-item';
 
 const UserReplies = ({ email }: { email: string }) => {
   const { data, isLoading } = usePosts(email, 'replies');
+  const { user } = useUser(email);
 
   const repliedPosts = data as unknown as PostType[];
 
@@ -23,10 +27,15 @@ const UserReplies = ({ email }: { email: string }) => {
     />
   ) : (
     <ul role='user-replies'>
-      {repliedPosts?.map((post) => (
-        <li key={post.id}>
-          <p>{post.replies[0].content}</p>
-        </li>
+      {repliedPosts?.map((post, index) => (
+        <>
+          <li key={post.id}>
+            {user && <RepliedPostItem post={post} user={user} />}
+            {index !== repliedPosts.length && (
+              <Separator className='mb-6 mt-4' />
+            )}
+          </li>
+        </>
       ))}
     </ul>
   );

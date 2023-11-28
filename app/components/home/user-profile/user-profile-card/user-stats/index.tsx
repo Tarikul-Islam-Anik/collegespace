57c5a4bd3605117d1ec8e052bb2bf8ea6c5201d4
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-11-28 19:02:34 +0600

    Refactor user stats component

--- app/components/home/user-profile/user-profile-card/user-stats/index.tsx
+++ app/components/home/user-profile/user-profile-card/user-stats/index.tsx
@@ -1,15 +1,7 @@
-import useUser from '@/hooks/useUser';
-import { Flex } from '@/components/layout/flex';
-import FollowersCount from './followers-count';
+import FollowCounts from './follow-counts';
 
 const UserStats = () => {
-  const { user } = useUser();
-
-  return (
-    <Flex direction='column' className='max-h-16'>
-      <FollowersCount id={user?.id!} count={user?._count?.followers!} />
-    </Flex>
-  );
+  return <FollowCounts />;
 };
 
 UserStats.displayName = 'UserStats';

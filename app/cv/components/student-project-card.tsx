629627f573ee23458a8f729838a7dc89564d67ee
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-05 23:47:06 +0600

    Add conditional rendering for project details in StudentProjectCard component

--- app/cv/components/student-project-card.tsx
+++ app/cv/components/student-project-card.tsx
@@ -2,6 +2,7 @@ import { Project } from '@/lib/type';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import StudentAboutInfoItem from './student-about-item';
+import { Text } from '@/components/typography/text';
 
 const StudentProjectCard = ({ projects }: { projects?: Project[] }) => {
   const sortedProjects = projects?.sort(
@@ -14,20 +15,35 @@ const StudentProjectCard = ({ projects }: { projects?: Project[] }) => {
       </CardHeader>
       <CardContent className='space-y-4'>
         <ul role='list'>
-          {sortedProjects?.map((project, index) => (
-            <li key={project.title}>
-              <StudentAboutInfoItem
-                title={project.title}
-                description={project.description}
-                link={project.website}
-                startDate={project.startDate}
-                endDate={project.endDate}
-              />
-              {index !== sortedProjects.length - 1 && (
-                <Separator className='my-4' />
-              )}
+          {sortedProjects?.length! > 0 ? (
+            <>
+              {sortedProjects?.map((project, index) => (
+                <li key={project.title}>
+                  <StudentAboutInfoItem
+                    title={project.title}
+                    description={project.description}
+                    link={project.website}
+                    startDate={project.startDate}
+                    endDate={project.endDate}
+                  />
+                  {index !== sortedProjects.length - 1 && (
+                    <Separator className='my-4' />
+                  )}
+                </li>
+              ))}
+            </>
+          ) : (
+            <li>
+              <Text
+                as='p'
+                size='sm'
+                className='text-center'
+                color='muted-foreground'
+              >
+                No project details has been added by the applicant.
+              </Text>
             </li>
-          ))}
+          )}
         </ul>
       </CardContent>
     </Card>

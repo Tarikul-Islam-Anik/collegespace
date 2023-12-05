67c1ed7a4efeff592b336d51086c36b1081fd3ee
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-05 23:46:58 +0600

    Add conditional rendering for experience details in StudentExperienceCard

--- app/cv/components/student-experience-card.tsx
+++ app/cv/components/student-experience-card.tsx
@@ -2,6 +2,7 @@ import { Experience, JobType } from '@/lib/type';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 import { Separator } from '@/components/ui/separator';
 import StudentAboutInfoItem from './student-about-item';
+import { Text } from '@/components/typography/text';
 const StudentExperienceCard = ({
   experiences,
 }: {
@@ -17,20 +18,35 @@ const StudentExperienceCard = ({
       </CardHeader>
       <CardContent className='space-y-4'>
         <ul role='list'>
-          {sortedExperiences?.map((experience, index) => (
-            <li key={experience.title}>
-              <StudentAboutInfoItem
-                title={experience.title}
-                description={experience.description}
-                jobType={experience.type as any}
-                startDate={experience.startDate}
-                endDate={experience.endDate}
-              />
-              {index !== sortedExperiences.length - 1 && (
-                <Separator className='my-4' />
-              )}
+          {sortedExperiences?.length! > 0 ? (
+            <>
+              {sortedExperiences?.map((experience, index) => (
+                <li key={experience.title}>
+                  <StudentAboutInfoItem
+                    title={experience.title}
+                    description={experience.description}
+                    jobType={experience.type as any}
+                    startDate={experience.startDate}
+                    endDate={experience.endDate}
+                  />
+                  {index !== sortedExperiences.length - 1 && (
+                    <Separator className='my-4' />
+                  )}
+                </li>
+              ))}
+            </>
+          ) : (
+            <li>
+              <Text
+                as='p'
+                size='sm'
+                className='text-center'
+                color='muted-foreground'
+              >
+                No experience details has been added by the applicant.
+              </Text>
             </li>
-          ))}
+          )}
         </ul>
       </CardContent>
     </Card>

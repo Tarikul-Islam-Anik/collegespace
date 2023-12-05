135cc3986d2958ff4aa1af5902628804af4971e0
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-05 13:21:22 +0600

    Refactor bounty item component

--- app/bounty-board/components/bounty-item.tsx
+++ app/bounty-board/components/bounty-item.tsx
@@ -1,34 +1,58 @@
+'use client';
+import { toast } from 'sonner';
 import { Flex } from '@/components/layout/flex';
 import { Heading } from '@/components/typography/heading';
 import { Text } from '@/components/typography/text';
 import { Button, buttonVariants } from '@/components/ui/button';
+import { format } from 'date-fns';
 
-const BountyItem = () => {
+const BountyItem = ({
+  title,
+  reward,
+  deadline,
+}: {
+  title: string;
+  reward: string;
+  deadline: string;
+}) => {
   return (
     <Flex justify='between' align='center' py={8} className='border-b'>
       <Flex align='center' gap={4}>
         <Heading
           as='h3'
-          size='xl'
+          size='sm'
           weight='medium'
-          className='max-w-[35rem] truncate'
+          className='w-72 md:w-full truncate md:text-xl'
         >
-          Bounty title
+          {title}
         </Heading>
-        <Text className={buttonVariants({ variant: 'secondary' })}>
-          Due on October 31, 2023
+        <Text
+          className={
+            buttonVariants({ variant: 'secondary' }) + ' hidden md:block'
+          }
+        >
+          Due on {format(new Date(deadline), 'MMM dd, yyyy')}
         </Text>
       </Flex>
-      <Flex align='center' justify='center' className='gap-9'>
+      <Flex align='center' justify='center' className='md:gap-9 gap-4'>
         <Flex direction='column'>
           <Text as='p' weight='medium'>
-            $25,000
+            {reward}
           </Text>
           <Text size='sm' className='ml-1 text-muted-foreground'>
             Bounty value
           </Text>
         </Flex>
-        <Button variant='outline'>Apply</Button>
+        <Button
+          variant='outline'
+          onClick={() => {
+            toast.error(
+              'You need to be logged in to apply for bounties. Login or create an account to continue.'
+            );
+          }}
+        >
+          Apply
+        </Button>
       </Flex>
     </Flex>
   );

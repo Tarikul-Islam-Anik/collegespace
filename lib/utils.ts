5aeacdd3f0a4ad58db13b3b5f96c3aae25999bf3
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-12 20:28:24 +0600

    Fix potential null reference in truncateString function

--- lib/utils.ts
+++ lib/utils.ts
@@ -39,5 +39,5 @@ export function getCountries(lang = 'en') {
 }
 
 export function truncateString(str: string, n: number) {
-  return str.length > n ? str.slice(0, n - 1) + '...' : str;
+  return str?.length > n ? str.slice(0, n - 1) + '...' : str;
 }

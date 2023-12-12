6c5eaa78877253c1e311ba869ea7778a3e476828
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-12 19:15:58 +0600

    Refactor job retrieval query in GET method.

--- app/api/company/jobs/route.ts
+++ app/api/company/jobs/route.ts
@@ -2,16 +2,17 @@ import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const jobs = await prisma.job.findMany({
-    include: {
-      company: {
-        select: {
-          name: true,
-          ownerId: true,
-        },
-      },
-    },
-  });
+  const jobs = await prisma.$queryRaw`
+  SELECT 
+    J.*, 
+    C.name AS company_name, 
+    C.ownerId AS company_ownerId
+  FROM 
+    collegespace.Job AS J
+  INNER JOIN 
+    collegespace.Company AS C ON J.companyId = C.id
+  ORDER BY
+    J.createdAt DESC;`;
 
   return NextResponse.json({ jobs: jobs }, { status: 200 });
 }

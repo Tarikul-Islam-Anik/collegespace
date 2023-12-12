1e6774ef2fd67e997ed8a29d1db24009a15d2508
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-12 19:15:18 +0600

    Refactor bounty route to use raw SQL query for improved performance.

--- app/api/bounty/route.ts
+++ app/api/bounty/route.ts
@@ -2,29 +2,18 @@ import prisma from '@/lib/prisma';
 import { NextRequest, NextResponse } from 'next/server';
 
 export async function GET(request: NextRequest) {
-  const bounties = await prisma.bounty.findMany({
-    select: {
-      title: true,
-      description: true,
-      reward: true,
-      deadline: true,
-      createdAt: true,
-      user: {
-        select: {
-          id: true,
-          name: true,
-          email: true,
-          image: true,
-          bio: true,
-          username: true,
-          createdAt: true,
-        },
-      },
-    },
-    orderBy: {
-      createdAt: 'desc',
-    },
-  });
+  const bounties = await prisma.$queryRaw`
+  SELECT 
+    B.*, 
+    U.name AS user_name, 
+    U.username AS user_username, 
+    U.image AS user_image
+  FROM
+    collegespace.Bounty AS B
+  INNER JOIN
+    collegespace.User AS U ON B.userId = U.id
+  ORDER BY
+    B.createdAt DESC;`;
 
   return NextResponse.json(bounties, { status: 200 });
 }

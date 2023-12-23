b73bd3d1a42a829ad13a5014329c8e44c12c9696
Tarikul-Islam-Anik <29859619+Tarikul-Islam-Anik@users.noreply.github.com>
2023-12-23 19:05:42 +0600

    Refactor GET function in jobs route to use Prisma ORM

--- app/api/company/jobs/route.ts
+++ app/api/company/jobs/route.ts
@@ -4,19 +4,19 @@ import { NextRequest, NextResponse } from 'next/server';
 export const revalidate = 1;
 
 export async function GET(request: NextRequest) {
-  const data = await prisma.$queryRaw`
-  SELECT 
-    J.*, 
-    C.name AS company_name, 
-    C.ownerId AS company_ownerId
-  FROM 
-    collegespace.Job AS J
-  INNER JOIN 
-    collegespace.Company AS C ON J.companyId = C.id
-  ORDER BY
-    J.createdAt DESC;`;
-
-  const jobs = { jobs: data };
+  const jobs = await prisma.job.findMany({
+    orderBy: {
+      createdAt: 'desc',
+    },
+    include: {
+      company: {
+        select: {
+          name: true,
+          ownerId: true,
+        },
+      },
+    },
+  });
 
   return NextResponse.json(jobs, { status: 200 });
 }
